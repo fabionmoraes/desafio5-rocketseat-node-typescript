903aad106437167a4e18fdb6d11b9d03f153dde2
Fabio Moraes <fabio@elaboredigital.com.br>
2020-07-26 16:02:49 -0300

    update 2020-07-26

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const filterIcome = this.transactions.filter(
+      item => item.type === 'income',
+    );
+    const filterOutcome = this.transactions.filter(
+      item => item.type === 'outcome',
+    );
+
+    const percorreIcome =
+      filterIcome.length > 0 ? filterIcome.map(item => item.value) : false;
+    const percorreOutcome =
+      filterOutcome.length > 0 ? filterOutcome.map(item => item.value) : false;
+
+    const somaIncome = percorreIcome
+      ? percorreIcome.reduce((accum, cur) => accum + cur)
+      : 0;
+    const somaOutcome = percorreOutcome
+      ? percorreOutcome.reduce((accum, cur) => accum + cur)
+      : 0;
+
+    const total = somaIncome - somaOutcome;
+
+    const object = {
+      income: somaIncome,
+      outcome: somaOutcome,
+      total,
+    };
+
+    return object;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
